b23bbbc0df66f022f9a1abc9c31e452176f085a8
christocarr <christocarr@outlook.com>
2020-06-12 12:55:59 +0100

    Add chat functionality

---
+++ public/index.js
@@ -0,0 +1,22 @@
+const chatLog = (chatText) => {
+  const chatBox = document.querySelector('.chat-log');
+  const chatLine = document.createElement('li');
+  chatLine.textContent = chatText;
+  chatBox.appendChild(chatLine);
+  // chatBox.scrollTop = chatBox.scrollHeight;
+};
+
+const onChatSubmitted = (ev) => {
+  ev.preventDefault()
+  const chatTextInput = document.querySelector('.chat-text');
+  const chatText = chatTextInput.value;
+  chatTextInput.value = '';
+  chatLog(chatText);
+};
+
+
+(() => {
+  chatLog('Welcome')
+  const chatForm = document.querySelector(chat-form)
+  chatForm.addEventListener('submit', onChatSubmitted)
+})
