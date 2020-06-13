29b3daedc85758dc99f9f3cc13594893b7e069bf
christocarr <christocarr@outlook.com>
2020-06-13 09:41:35 +0100

    Fix form submit event listener

--- public/index.js
+++ public/index.js
@@ -3,20 +3,19 @@ const chatLog = (chatText) => {
   const chatLine = document.createElement('li');
   chatLine.textContent = chatText;
   chatBox.appendChild(chatLine);
-  // chatBox.scrollTop = chatBox.scrollHeight;
+  chatBox.scrollTop = chatBox.scrollHeight;
 };
 
 const onChatSubmitted = (ev) => {
-  ev.preventDefault()
+  ev.preventDefault();
   const chatTextInput = document.querySelector('.chat-text');
   const chatText = chatTextInput.value;
   chatTextInput.value = '';
   chatLog(chatText);
 };
 
-
 (() => {
-  chatLog('Welcome')
-  const chatForm = document.querySelector(chat-form)
-  chatForm.addEventListener('submit', onChatSubmitted)
-})
+  chatLog('Welcome');
+  const chatForm = document.querySelector('.chat-form');
+  chatForm.addEventListener('submit', onChatSubmitted);
+})();
