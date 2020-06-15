6bf7a7865e373adcda29ef06f8d2a54a33ad53d1
christocarr <christocarr@outlook.com>
2020-06-15 10:42:45 +0100

    Add socket.io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 
 app.use(express.static(`${__dirname}/../public`));
 
 const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (sock) => {
+  console.log('connection')
+});
 
 server.on('error', (err) => {
   console.error(err);
