156e65782665ea0d885ae5e789281a9a53433e81
christocarr <christocarr@outlook.com>
2020-06-19 07:53:03 +0100

    Add game board grid to dom

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 const chatLog = (chatText) => {
-  console.log(chatText)
+  console.log(chatText);
   const chatBox = document.querySelector('.chat-log');
   const chatLine = document.createElement('li');
   chatLine.textContent = chatText;
@@ -12,11 +12,38 @@ const onChatSubmitted = (sock) => (ev) => {
   const chatTextInput = document.querySelector('.chat-text');
   const chatText = chatTextInput.value;
   chatTextInput.value = '';
-  
-  sock.emit('message', chatText)
+
+  sock.emit('message', chatText);
+};
+
+const getBoard = (canvas, numCells = 3) => {
+  const ctx = canvas.getContext('2d');
+  ctx.strokeRect(0, 0, 400, 400);
+  const cellSize = Math.floor(canvas.width / numCells);
+  const drawGrid = () => {
+    ctx.beginPath();
+    for (let i = 0; i < numCells; i++) {
+      ctx.moveTo(i * cellSize, 0);
+      ctx.lineTo(i * cellSize, cellSize * numCells);
+    }
+
+    for (let i = 0; i < numCells; i++) {
+      ctx.moveTo(0, i * cellSize);
+      ctx.lineTo(cellSize * numCells, i * cellSize);
+    }
+
+    ctx.stroke();
+  };
+
+  return { drawGrid };
 };
 
 (() => {
+  const canvas = document.querySelector('canvas');
+  const { fillCell, drawGrid } = getBoard(canvas);
+
+  drawGrid();
+
   const sock = io();
 
   sock.on('message', chatLog);
